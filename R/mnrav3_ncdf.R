dbf07720dfa199d1337f81c5aec80ff89c776954
Moritz Shore <moritzshore@gmail.com>
2024-09-26 20:35:01 +0200

    progress

--- R/mnrav3_ncdf.R
+++ R/mnrav3_ncdf.R
@@ -19,42 +19,79 @@ read_write_ncdf <- function(url, savefiles){
   # Download first file to get the dimensios set, then loop through following
   # Files
 
+  ## tODO remove
+  idate = 3
   # open first Netcdf file
-  ncin_crop <- nc_open_retry(url[1])
+  ncin_crop <- nc_open_retry(url[idate])
 
-  # getting numbers
-  # do once
+  # get dimensions
+  lat <- ncvar_get(ncin_crop, "y")
+  lon <- ncvar_get(ncin_crop, "x")
+  ydim <- dim(lat)
+  xdim <- dim(lon)
+
+  # defining dimensions
+  mytimedef <- ncdim_def(name = "time", units = "hour", vals =1, unlim = F)
+  myxdef <- ncdim_def(name = "y", units = "m",longname = "projection_y_coordinate",vals = ydim)
+  myydef <- ncdim_def(name = "x", units = "m",longname = "projection_x_coordinate", vals =xdim)
+
+  # define UTMS
+  lat_vals <- ncvar_def("lat_vals", units = "deg", dim = list(myydef,myxdef, mytimedef))
+  lon_vals <- ncvar_def("lon_vals", units = "deg", dim = list(myydef,myxdef, mytimedef))
 
   varlist = ncin_crop$var %>% names()
   varnr = length(varlist)
   nc_var_list = list()
+  nc_def_list = list()
+
 
   for (var_index in 1:varnr) {
-    cat("Getting:", varlist[var_index], "\n")
-    print(ncvar_get(ncin_crop, varlist[var_index]))
+
+    # extract variable data
+    cat(blue("extracting"), underline(varlist[var_index]), blue("data"),"\n", sep = " ")
+    nc_var_list[[var_index]] <- ncvar_get(ncin_crop, varlist[var_index])
+
+    # extract variable attributes
+    cat(magenta("extracting"), underline(varlist[var_index]), magenta("attributes"),"\n", sep = " ")
+    var_attr <- ncatt_get(ncin_crop, varlist[var_index])
+    print(var_attr$standard_name)
+    # define a variable defintion based on the extracted attributes
+    # TODO could add chunk sizes which are present on variables that arent lat/long?
+    cat(cyan("defining"), underline(varlist[var_index]), cyan("attributes"),"\n", sep = " ")
+    var_def <- ncvar_def(
+      var_attr$standard_name,
+      units = var_attr$units,
+      list(myydef,myxdef, mytimedef)
+    )
+
+    # add the definition to the list
+    nc_attr_list[[var_index]] <- var_def
+
 
   }
+  names(nc_var_list) <- varlist
+  names(nc_attr_list) <- varlist
 
-  lat <- ncvar_get(ncin_crop, "latitude")
-  lon <- ncvar_get(ncin_crop, "longitude")
-  at2<-ncvar_get(ncin_crop, "air_temperature_2m")
+  filename = savefiles[idate]
+  # create NC file with the attribute definition list
+  cat(yellow("creating"), underline(filename), yellow("on disc"),"\n", sep = " ")
+  to_write_nc <- nc_create(filename, vars = nc_attr_list)
+
+
+  for (var_index in 1:varnr) {
+    cat(green("writing"), underline(varlist[var_index]), green("to file"),"\n", sep = " ")
+    ncvar_put(nc = to_write_nc, varid = varlist[var_index], vals = nc_var_list[[var_index]])
+  }
+
+
+  cat(yellow("saving"), underline(filename), yellow("data"),"\n", sep = " ")
+  cat(yellow("closing file #"), underline(idate),"\n", sep = " ")
 
   nc_close(ncin_crop)
-  # dim(lat)[2]
-  # dim(lat)[1]
-  # defning dims
-  mytime <- ncdim_def(name = "time", units = "hour", vals =1, unlim = F)
-  mylat <- ncdim_def(name = "y", units = "m",longname = "projection_y_coordinate", vals = 1:dim(lat)[2])
-  mylon <- ncdim_def(name = "x", units = "m",longname = "projection_y_coordinate", vals = 1:dim(lat)[1])
 
-  # define
-  lat_vals <- ncvar_def("lat_vals", units = "deg", dim = list(mylat,mylon, mytime))
-  lon_vals <- ncvar_def("lon_vals", units = "deg", dim = list(mylat,mylon, mytime))
-  at2_defined <- ncvar_def("temp", units = "K", dim = list(mylat,mylon, mytime))
+
 
   # write
-  filename = savefiles[3]
-  helloworld <- nc_create(filename, vars = list(lat_vals, lon_vals, at2_defined))
 
   ncvar_put(helloworld, "temp", vals = at2)
   ncvar_put(helloworld, "lat_vals", vals = lat)
