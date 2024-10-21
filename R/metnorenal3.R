a8088f9815495513334eacf487cdd9183f04075b
Moritz Shore <moritzshore@gmail.com>
2024-10-21 13:47:23 +0200

    docs and printing

--- R/metnorenal3.R
+++ R/metnorenal3.R
@@ -85,7 +85,7 @@
 #' @param continue if `ncdf` is `TRUE`, then you can pass a folder name (in passed directory) here to continue an aborted download session.
 #' @importFrom abind abind
 #' @importFrom dplyr nth mutate %>% tibble
-#' @importFrom lubridate year month day hour
+#' @importFrom lubridate year month day hour as_datetime
 #' @importFrom mapview mapview
 #' @importFrom ncdf4 nc_open ncvar_get nc_close
 #' @importFrom purrr map
@@ -1280,7 +1280,7 @@ nc_open_retry <- function(link) {
   if (nc_file %>% length() > 1) {
     return(nc_file)
   } else{
-    print("retry download w/out radiation")
+    mt_print(TRUE, "nc_open_retry", "retrying donwload with out longwave radiation")
     # https://github.com/metno/NWPdocs/wiki/MET-Nordic-dataset#parameters
     # find the location in the link where longwave radiation is, and remove it,
     # then try to open the file wihtout this variable.
@@ -1295,10 +1295,11 @@ nc_open_retry <- function(link) {
     }
 
   if (length(new_nc_file)>1) {
+    mt_print(TRUE, "nc_open_retry", "download sans longradiation succeeded!")
     warning("file missing longwave radiation")
     return(new_nc_file)
   } else{
-    print("retry download..")
+    mt_print(TRUE, "nc_open_retry", "retrying donwload..")
     attempt = 1
     while ((attempt < 10) & (length(nc_file) == 1)) {
       Sys.sleep(5)
@@ -1310,7 +1311,7 @@ nc_open_retry <- function(link) {
     }
 
     if (length(nc_file) > 1) {
-      print("connection re-established!")
+      mt_print(TRUE, "nc_open_retry", "connection re-established!")
       return(nc_file)
     } else{
       stop("download failed after 10 attempts.")
