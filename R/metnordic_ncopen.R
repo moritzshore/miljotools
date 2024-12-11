83e21764722f02042af698e05905640a93c6af18
Moritz Shore <moritzshore@gmail.com>
2024-12-11 16:37:44 -0500

    Update metnordic_ncopen.R
    removing the solar radiation stuf

--- R/metnordic_ncopen.R
+++ R/metnordic_ncopen.R
@@ -11,43 +11,21 @@ nc_open_stable <- function(link) {
   if (nc_file %>% length() > 1) {
     return(nc_file)
   } else{
-    mt_print(TRUE, "nc_open_retry", "retrying donwload with out longwave radiation")
-
-    # https://github.com/metno/NWPdocs/wiki/MET-Nordic-dataset#parameters
-    # find the location in the link where longwave radiation is, and remove it,
-    # then try to open the file wihtout this variable.
-    split = link %>% stringr::str_split(",", simplify = T)
-    longwave_index <- grepl(x = split, pattern =  "longwave") %>% which()
-    new_link = paste(split[-longwave_index], collapse = ",")
-    new_nc_file <- tryCatch(
-      expr = {ncdf4::nc_open(new_link)},
-      error = function(cond) {
-        warning("failed..", cond, "retry!")
-        return(NA)})
-  }
-
-  if (length(new_nc_file)>1) {
-    mt_print(TRUE, "nc_open_retry", "download sans longradiation succeeded!")
-    warning("file missing longwave radiation")
-    return(new_nc_file)
-  } else{
-    mt_print(TRUE, "nc_open_retry", "retrying donwload..")
-
+    mt_print(TRUE, "nc_open_retry", crayon::red("retrying donwload.."))
     attempt = 1
     while ((attempt < 10) & (length(nc_file) == 1)) {
-      Sys.sleep(5)
+      Sys.sleep(2)
       attempt = attempt + 1
       nc_file <- tryCatch(expr = {ncdf4::nc_open(link)},
                           error = function(cond) {
-                            warning("failed..", cond, "retry!")
+                            warning("failed again..", cond, "retry #", attempt)
                             return(NA)})
     }
-
     if (length(nc_file) > 1) {
       mt_print(TRUE, "nc_open_retry", "connection re-established!")
       return(nc_file)
     } else{
-      stop("download failed after 10 attempts.")
+      warning("download failed after 10 attempts..")
     }
   }
 }
