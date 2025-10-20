ff403f92616334da39a08ea314726e55b6961b90
Moritz Shore <moritzshore@gmail.com>
2025-10-20 16:04:40 +0200

    implement `ltc` dataset

--- R/metnordic_buildquery.R
+++ R/metnordic_buildquery.R
@@ -15,8 +15,8 @@
 #' @param fromdate ie. "2019-01-01 00:00:00"
 #' @param todate ie. "2020-12-31 23:00:00"
 #' @param dataset either 'reanalysis' for the re-run archive, 'operational' for
-#'   the operational archive, or 'continuous' to source from both, depending on
-#'   time range.
+#'   the operational archive, 'ltc' for the EXPERIMENTAL long term consistent product or 'continuous' to source from all, depending on
+#'   date range.
 #' @param grid_resolution an integer, ie. 3 for 3x3 km grid.
 #' @param verbose print to console?
 #'
@@ -139,12 +139,25 @@ metnordic_buildquery <- function(bounding_coords, mn_variables, fromdate, todate
     if(lubridate::as_datetime(todate) > lubridate::as_datetime( Sys.time())){
       warning("`todate` lies in the future... the operational forecast might not exist for this time yet???")
     }
-    if(lubridate::as_datetime(fromdate) < lubridate::as_datetime("2012-09-01 03:00:00")){
-      stop("Reanalysis 3 only starts '2012-09-01 03:00:00', contact maintainer if this changes..")
+    if(lubridate::as_datetime(fromdate) < lubridate::as_datetime("1958-08-02 00:00:00")){
+       warning("Dataset 'longterm consistent' (ltc) only starts @ '1958-08-02 00:00:00'. Trying to download these queries may result in an error!")
     }
-  }else{
-    stop(paste0("dataset = '", dataset, "' not supported or not recognized. Please pass either dataset = 'continuous', reanaysis' or 'operational'"))
-  }
+  }else if (dataset == "ltc"){
+    if(lubridate::as_datetime(todate) >= lubridate::as_datetime("2012-09-01 03:00:00")){
+      warning("Your request for 'ltc' data includes timestamps which are covered by the (non-experimental) reanalysis product. Perhaps you should set the `dataset` parameter to 'continuous'?")
+    }
+    if(lubridate::as_datetime(fromdate) < lubridate::as_datetime("1958-08-02 00:00:00")){
+       warning("Dataset 'longterm consistent' (ltc) only starts @ '1958-08-02 00:00:00'. Trying to download these queries may result in an error!")
+    }
+    }else{
+      stop(
+        paste0(
+          "dataset = '",
+          dataset,
+          "' not supported or not recognized. Please pass either dataset = 'continuous', reanaysis', 'operational', or 'ltc'"
+        )
+      )
+    }
 
   daterange <- seq(fromdate_lubed,todate_lubed , by="hour")
   years <- lubridate::year(daterange)
@@ -153,24 +166,48 @@ metnordic_buildquery <- function(bounding_coords, mn_variables, fromdate, todate
   hours <- lubridate::hour(daterange) %>% stringr::str_pad(width = 2, side = "left", pad = "0")
 
   # header for thredds server
-  filenames <- paste0("met_analysis_1_0km_nordic_", years, months, days, "T", hours, "Z", ".nc")
-  filepath <- paste0(years, "/", months,"/",days,"/")
+  file_header_ltc <- "met_analysis_ltc_1_0km_nordic_"
+  file_header_nordic <- "met_analysis_1_0km_nordic_"
 
-  if(dataset == "reanalysis"){
-    header = "https://thredds.met.no/thredds/dodsC/metpparchivev3/"
-    full_urls <- paste0(header, filepath, filenames, "?", var_query)
-  }else if(dataset == "operational"){
-    header = "https://thredds.met.no/thredds/dodsC/metpparchive/"
-    full_urls <- paste0(header, filepath, filenames, "?", var_query)
+  if(dataset == "ltc"){
+    filenames <- paste0(file_header_ltc, years, months, days, "T", hours, "Z", ".nc")
+  }else if(dataset %in% c("reanalysis", "operational")){
+    filenames <- paste0(file_header_nordic, years, months, days, "T", hours, "Z", ".nc")
   }else if(dataset == "continuous"){
-    # seperated based off cut-off.
-    re_header = "https://thredds.met.no/thredds/dodsC/metpparchivev3/"  # rerun = v3
-    op_header =  "https://thredds.met.no/thredds/dodsC/metpparchive/"
+    filenames_ltc <- paste0(file_header_ltc, years, months, days, "T", hours, "Z", ".nc")
+    filenames_mn <- paste0(file_header_nordic, years, months, days, "T", hours, "Z", ".nc")
+    ltc_header_split <-  (lubridate::as_datetime(daterange) < lubridate::as_datetime("2012-09-01 03:00:00")) %>% which()
+    mn_header_split <-  (lubridate::as_datetime(daterange) >= lubridate::as_datetime("2012-09-01 03:00:00")) %>% which()
+    filenames <- c(filenames_ltc[ltc_header_split],filenames_mn[mn_header_split])
+  }else{
+    stop("dataset not understood..")
+  }
 
-    ## WARNING this will need to change if metnordic ever updates the re-run.
-    ## could replace with an optional parameter to have users define the cutoff..
-    reanal_split <- (lubridate::as_datetime(daterange) <= lubridate::as_datetime("2023-01-31 23:00:00")) %>% which()
-    op_split <- (lubridate::as_datetime(daterange) > lubridate::as_datetime("2023-01-31 23:00:00")) %>% which()
+  filepath <- paste0(years, "/", months,"/",days,"/")
+  re_header = "https://thredds.met.no/thredds/dodsC/metpparchivev3/"
+  op_header =  "https://thredds.met.no/thredds/dodsC/metpparchive/"
+  ltc_header =  "https://thredds.met.no/thredds/dodsC/metppltcarchivev1/"
+  if(dataset == "reanalysis"){
+    full_urls <- paste0(re_header, filepath, filenames, "?", var_query)
+  }else if(dataset == "operational"){
+    full_urls <- paste0(op_header, filepath, filenames, "?", var_query)
+  }else if(dataset == "ltc"){
+    full_urls <- paste0(ltc_header, filepath, filenames, "?", var_query)
+    }else if(dataset == "continuous"){
+      # separated based off cut-off.
+      ## WARNING this will need to change if metnordic ever updates the re-run.
+      ## could replace with an optional parameter to have users define the cutoff..
+
+      ltc_split <-  (lubridate::as_datetime(daterange) < lubridate::as_datetime("2012-09-01 03:00:00")) %>% which()
+      reanal_split <- ((lubridate::as_datetime(daterange) <= lubridate::as_datetime("2023-01-31 23:00:00")) &
+        (lubridate::as_datetime(daterange) >= lubridate::as_datetime("2012-09-01 03:00:00"))) %>% which()
+      op_split <- (lubridate::as_datetime(daterange) > lubridate::as_datetime("2023-01-31 23:00:00")) %>% which()
+
+    if(length(ltc_split) > 0){
+      ltc_full_urls <-  paste0(ltc_header, filepath[ltc_split], filenames[ltc_split], "?", var_query)
+    }else{
+      ltc_full_urls <- c()
+    }
 
     if(length(reanal_split)>0){
       re_full_urls <- paste0(re_header, filepath[reanal_split], filenames[reanal_split], "?", var_query)
@@ -183,8 +220,7 @@ metnordic_buildquery <- function(bounding_coords, mn_variables, fromdate, todate
     }else{
       op_full_urls <- c()
     }
-
-    full_urls <- c(re_full_urls,op_full_urls)
+    full_urls <- c(ltc_full_urls, re_full_urls, op_full_urls)
   }else{
     stop("parameter `dataset` not recognized!")
   }
